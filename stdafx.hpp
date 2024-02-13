0c67059988761b7cb9a7929aff3abc0f0952470d
Fabio Rossini Sluzala <fabio3rs@gmail.com>
2024-02-12 22:38:35 -0300

    Fix missing stdafx.hpp

---
+++ stdafx.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "simdjson.h"
+#include <any>
+#include <chrono>
+#include <cstdlib>
+#include <dlfcn.h>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <string_view>
+#include <unistd.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
